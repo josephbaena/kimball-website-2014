7cb3dd656a54f75d2b3d1a6a8f957c2864018660
Jorge Aguirre <jag20101@stanford.edu>
2014-09-07 21:13:58 -0700

    preassign folder changes

---
+++ preassigns/events.json.php
@@ -0,0 +1,15 @@
+{
+	"success": 1,
+	"result": [
+		{
+			"id": "293",
+			"title": "Sample Event",
+			"staff": "Jorge Aguirre",
+			"location": "Main Lounge",
+			"points": 3,
+			"class": "event-success",
+			"start": "1362938400000",
+			"end":   "1363197686300"
+		}
+	]
+}
